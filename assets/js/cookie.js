7e94354538a2e76b43f9821b24cf3e981c60d09c
OmidDeiminiyat <omid.deiminiyat@yahoo.com>
2024-07-08 18:37:02 +0200

    Login Cookie

--- assets/js/cookie.js
+++ assets/js/cookie.js
@@ -1,27 +1,69 @@
 
+
+
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  const userEmail = document.getElementById('username').value;
+  const password = document.getElementById('password').value;
+  const errorMessage = document.getElementById('errorMessage');
+
+  // Sample credentials for demonstration purposes
+
+  fetch('https://bfrnndgsghbkfrbbzuuk.supabase.co/rest/v1/Subscribe', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      'apikey': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJmcm5uZGdzZ2hia2ZyYmJ6dXVrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTQ0MjA0NzcsImV4cCI6MjAyOTk5NjQ3N30.2fBDWPexcBP6OfB0pY11Me1N5xzioXkc4agte3buhUU' // Replace 'your-api-key' with your actual Supabase API key
+    }
+  })
+  .then(response => response.json())
+  .then(NewReciData => {
+    const Emails = userEmail;
+    const checkEmail = NewReciData.filter(item => item.email === Emails);
+    checkEmail.sort((a, b) => {
+      if (a.name < b.name) {
+        return -1;
+      }
+      if (a.name > b.name) {
+        return 1;
+      }
+      return 0;
+    });
+     console.log('Email is:' + checkEmail[0].email); // Do something with the fetched data
+     if (Emails === checkEmail[0].email && password === checkEmail[0].Password) {
+      onLoginSuccess()
+         
+      alert('Login successful!');
+      // Redirect to another page or perform other actions here
+  } else {
+      errorMessage.textContent = 'Invalid username or password';
+  }
+  })
+  .catch(error => {
+    console.error('Error fetching data:', error);
+  });
+  
+});
+
 function setCookie(name, value, days) {
-    const date = new Date();
-    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-    const expires = "expires=" + date.toUTCString();
-    document.cookie = name + "=" + value + ";" + expires + ";path=/;Secure;SameSite=Strict";
-    console.log('cookie');
-  }
-  
+  const date = new Date();
+  date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+  const expires = "expires=" + date.toUTCString();
+  document.cookie = name + "=" + value + ";" + expires + ";path=/;Secure;SameSite=Strict";
+  console.log('cookie');
+}
 
-  const users = 'Kandlus';
-  const code = '37727'
-  const Email = 'omid@yahoo.cpm';
+const users = 'Kandlus-users';
+const code = 'omid@yahoo.com';
+const Email = 'omid@yahoo.com';
 
-  // Example usage: Set the auth token when the user logs in
-  function onLoginSuccess(authToken) {
-    setCookie(users, code, 3); // Set cookie for 3 days
-  }
-  
-  if (users == 'Kandlus') {
-    onLoginSuccess()
-  }
+// Example usage: Set the auth token when the user logs in
 
 
+function onLoginSuccess(authToken) {
+  setCookie(users, code, 3); // Set cookie for 3 days
+}  
 
 
   // Function to get a cookie by name
@@ -38,10 +80,10 @@ function getCookie(name) {
   }
   
   function checkAutoLogin() {
-    const authToken = getCookie('Kandlus');
+    const authToken = getCookie('Kandlus-users');
     
     if (authToken) {
-fetch(`https://bfrnndgsghbkfrbbzuuk.supabase.co/rest/v1/Subscribe?Code=eq.${authToken}`, {
+fetch(`https://bfrnndgsghbkfrbbzuuk.supabase.co/rest/v1/Subscribe?email=eq.${authToken}`, {
     method: 'GET',
     headers: {
       'Content-Type': 'application/json',
@@ -56,7 +98,7 @@ fetch(`https://bfrnndgsghbkfrbbzuuk.supabase.co/rest/v1/Subscribe?Code=eq.${auth
         autoLoginUser(dataAst[0]);
      } else {
         // Token is invalid, delete the cookie
-        setCookie('Kandlus', '', -1);
+        setCookie('Kandlus-users', '', -1);
       }
   })
   .catch(error => {
@@ -73,9 +115,37 @@ fetch(`https://bfrnndgsghbkfrbbzuuk.supabase.co/rest/v1/Subscribe?Code=eq.${auth
     console.log('welcome:' + sessionName);
     const username = document.getElementById('username');
     username.innerText = 'Welcome: ' + sessionName;
-    const userExist = document.getElementById('right-s');
-    userExist.style.display = 'block';
+    //const userExist = document.getElementById('right-s');
+    // userExist.style.display = 'block';
  
   }
   document.addEventListener('DOMContentLoaded', checkAutoLogin);
 
+
+
+
+
+
+
+  function letCookie(name, value, days) {
+    const date = new Date();
+    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+    const expires = "expires=" + date.toUTCString();
+    document.cookie = name + "=" + value + ";" + expires + ";path=/;Secure;SameSite=Strict";
+    console.log('cookie');
+  }
+  
+  const guest = 'Kandlus-guest';
+  const guestCode = '8393';
+  
+  // Example usage: Set the auth token when the user logs in
+  
+  
+  function guestVisit() {
+    letCookie(guest, guestCode, 3); // Set cookie for 3 days
+  }  
+
+
+  guestVisit()
+
+ 
