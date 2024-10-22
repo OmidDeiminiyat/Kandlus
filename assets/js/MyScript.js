542d3bdcd90224d585740577595caf37ba9d7fcf
OmidDeiminiyat <omid.deiminiyat@yahoo.com>
2024-10-22 19:19:59 +0200

    personal

--- assets/js/MyScript.js
+++ assets/js/MyScript.js
@@ -53,13 +53,14 @@ var RLink = document.getElementById('listen-8');
 var personalLinks = document.getElementById('personNewLink');
 var PersonNewText = document.getElementById('persoTx');
 var numerologyLink = document.getElementById('IdNumerology');
+var TextNumerology = document.getElementById('numerologyText');
 // var tarotLink = document.getelementById('listenTarot');
 var matchLink = document.getElementById('listenMatch');
 var textDat = document.getElementById('dateText');
 
 
 
-    console.log(PhDatas[1].Standard);
+    console.log(PhDatas[0].Pro);
     console.log(sessionName);
     
 
@@ -70,7 +71,7 @@ var textDat = document.getElementById('dateText');
       Tartots.innerText = 'To use astrology services, you must log in and select one of the plans';
       Yogik.innerText = 'To use astrology services, you must log in and select one of the plans';
       phisicalText.innerText = 'To use astrology services, you must log in and select one of the plans';
-      numerologyLink.innerHTML = 'To use astrology services, you must log in and select one of the plans';
+      TextNumerology.innerHTML = 'To use astrology services, you must log in and select one of the plans';
       TablN.innerText = 'To use astrology services, you must log in and select one of the plans';
       Radic.innerText = 'To use astrology services, you must log in and select one of the plans';
       textDat.innerText = 'To use astrology services, you must log in and select one of the plans';
@@ -82,49 +83,60 @@ var textDat = document.getElementById('dateText');
       Tartots.innerText = 'This service is available for plan: Pro';
       Yogik.innerText = 'This service is available for plans: Standard and Pro';
       phisicalText.innerText = 'This service is available for plan: Pro';
-      numerologyLink.innerHTML = 'This service is available for plans: Standard and Pro';
+      TextNumerology.innerHTML = 'This service is available for plans: Standard and Pro';
       TablN.innerText = 'This service is available for plan: Pro';
       Radic.innerText = 'This service is available for plan: Pro';
       textDat.innerText = 'This service is available for plan: Pro';
       PersonNewText.innerText = 'This service is available for plan: Pro';
     }
     
-    else if (sessionName === PhDatas[2].Basic) {
-  Bios.innerText =  'This service is available for Standard and Pro plans';
-  HoroFir.innerHTML = 'This service is available for Standard and Pro plans';
-  assenD.innerHTML = 'This service is available for Standard and Pro plans';
-} else if(sessionName != PhDatas[2].Basic) {
-  Bios.innerHTML = 'Let calculate your birth day and time to know your biorythm circle';
-  bioLinks.style.display = 'block';
-  HoroFir.innerHTML = 'Daily, Weekly, Monthly Horoscopes. Annual Forecasts, Transits, Progressions, Relationship, Compatibility and more';
-  HorLink.style.display = 'block';
-  assenD.innerHTML = 'Know your Personality, Appearance, Behavior, Attitudes, Life Path, Identity and more';
-  AssLin.style.display = 'block';
-} else if(sessionName === PhDatas[2].Basic || sessionName === PhDatas[1].Standard) {
-  Tartots.innerText = 'This service is available for plan Pro'
-Yogik.innerText = 'This service is available for plan Pro'
-TablN.innerText = 'This service is available for plan Pro'
-Radic.innerText = 'This service is available for plan Pro'
-textDat.innerText = 'This service is available for plan Pro';
-phisicalText.innerText = 'This service is available for plan Pro';
-PersonNewText.innerText = 'This service is available for plan Pro';
-} else if(sessionName != PhDatas[2].Basic || sessionName != PhDatas[1].Standard){
-  Tartots.innerText = 'This service tells you your traits by considering the position of the planets and constellations at the time of your birth in that geographical location'
-PersonLink.style.display = 'block';
-personalNew.style.display = 'block';
-Yogik.innerText = 'These yogas are formed based on the placement of planets in relation to each other and to certain houses in the natal chart'
-yogiLink.style.display = 'block';
-TablN.innerText = 'A numerology chart is created using specific numbers derived from a person’s birth date and full name. The chart provides insights into various aspects of their life.'
-tabLink.style.display = 'block';
-Radic.innerText = 'The radical number or root number, also known as the life path number, is derived from the date of birth. By having your root number, you can be aware of your life path'
-RLink.style.display = 'block';
-textDat.innerText = 'This service helps you find out if you are compatible with someone you want to marry';
-matchLink.style.display = 'block';
-phisicalText.innerText = 'This service tells you the most suitable yoga exercises according to the position of the planets at the time of your birth and can teach you yoga at the same time';
-phisicaLink.style.display = 'block';
-PersonNewText.innerText = 'This service tells you your traits by considering the position of the planets and constellations at the time of your birth in that geographical location';
-personalLinks.style.display = 'block';
-} 
+    else if (sessionName === PhDatas[1].Standard) {
+      Bios.innerHTML = 'Let calculate your birth day and time to know your biorythm circle';
+      bioLinks.style.display = 'block';
+      HoroFir.innerHTML = 'Daily, Weekly, Monthly Horoscopes. Annual Forecasts, Transits, Progressions, Relationship, Compatibility and more';
+      HorLink.style.display = 'block';
+      assenD.innerHTML = 'Know your Personality, Appearance, Behavior, Attitudes, Life Path, Identity and more';
+      AssLin.style.display = 'block';
+      TextNumerology.innerText = 'Get your personal day number and meaning of your personal number.';
+      numerologyLink.style.display = 'block';
+      Yogik.innerText = 'There are many types of Yogas in astrology, where each Yoga can result in different effects on a person life, such as prosperity, success, hardships, relationships, or spiritual growth.';
+      yogiLink.style.display = 'block';
+      TablN.innerText = 'This service is available for plan: Pro';
+      Radic.innerText = 'This service is available for plan: Pro';
+      textDat.innerText = 'This service is available for plan: Pro';
+      PersonNewText.innerText = 'This service is available for plan: Pro';
+      TablN.innerText = 'This service is available for plan: Pro';
+      Radic.innerText = 'This service is available for plan: Pro';
+      textDat.innerText = 'This service is available for plan: Pro';
+      PersonNewText.innerText = 'This service is available for plan: Pro';
+      phisicalText.innerText = 'This service is available for plan: Pro';
+      Tartots.innerText = 'This service is available for plan: Pro';
+      } else if(sessionName === PhDatas[0].Pro) {        
+        Tartots.innerText = 'This service tells you your traits by considering the position of the planets and constellations at the time of your birth in that geographical location'
+        PersonLink.style.display = 'block';
+        Yogik.innerText = 'These yogas are formed based on the placement of planets in relation to each other and to certain houses in the natal chart'
+        yogiLink.style.display = 'block';
+        TablN.innerText = 'A numerology chart is created using specific numbers derived from a person’s birth date and full name. The chart provides insights into various aspects of their life.'
+        tabLink.style.display = 'block';
+        Radic.innerText = 'The radical number or root number, also known as the life path number, is derived from the date of birth. By having your root number, you can be aware of your life path'
+        RLink.style.display = 'block';
+        textDat.innerText = 'This service helps you find out if you are compatible with someone you want to marry';
+        matchLink.style.display = 'block';
+        phisicalText.innerText = 'This service tells you the most suitable yoga exercises according to the position of the planets at the time of your birth and can teach you yoga at the same time';
+        phisicaLink.style.display = 'block';
+        PersonNewText.innerText = 'This service tells you your traits by considering the position of the planets and constellations at the time of your birth in that geographical location';
+        personalLinks.style.display = 'block';
+        Bios.innerHTML = 'Let calculate your birth day and time to know your biorythm circle';
+        bioLinks.style.display = 'block';
+        HoroFir.innerHTML = 'Daily, Weekly, Monthly Horoscopes. Annual Forecasts, Transits, Progressions, Relationship, Compatibility and more';
+        HorLink.style.display = 'block';
+        assenD.innerHTML = 'Know your Personality, Appearance, Behavior, Attitudes, Life Path, Identity and more';
+        AssLin.style.display = 'block';
+        TextNumerology.innerText = 'Get your personal day number and meaning of your personal number.';
+        numerologyLink.style.display = 'block';
+        Yogik.innerText = 'There are many types of Yogas in astrology, where each Yoga can result in different effects on a person life, such as prosperity, success, hardships, relationships, or spiritual growth.';
+        yogiLink.style.display = 'block';
+      } 
 
 if(!sessionName || sessionName === PhDatas[2].Basic || sessionName === PhDatas[1].Standard) {
   console.log('its empty');
--- assets/js/service-worker.js
+++ assets/js/service-worker.js
@@ -1,43 +1,44 @@
+const CACHE_NAME = 'my-cache-v2';  
 
+// Files to cache
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/assets/css/style.css',
+  '/assets/js/script.js'
+];
 
+// Install Service Worker and Cache Files
 self.addEventListener('install', function(event) {
-    // Perform install steps
     event.waitUntil(
-        caches.open('my-cache').then(function(cache) {
+        caches.open(CACHE_NAME).then(function(cache) {
             console.log('Opened cache');
-            return cache.addAll([
-                '/',
-                '/index.html',
-                '/assets/css/style.css',
-                '/assets/js/script.js'
-            ]);
+            return cache.addAll(urlsToCache);
         })
     );
+    self.skipWaiting(); 
 });
 
-
-
+// Fetch Event to Serve Cached Files
 self.addEventListener('fetch', function(event) {
     event.respondWith(
         caches.match(event.request).then(function(response) {
-            // Cache hit - return response
             if (response) {
-                return response;
+                return response; // Serve the cached response if available
             }
-            // Clone the request to avoid consuming it
+            // Clone the request and fetch it from the network
             var fetchRequest = event.request.clone();
 
             return fetch(fetchRequest).then(
                 function(response) {
-                    // Check if we received a valid response
                     if (!response || response.status !== 200 || response.type !== 'basic') {
-                        return response;
+                        return response; // Return network response if invalid or not a basic response
                     }
 
-                    // Clone the response to avoid consuming it
+                    // Clone the response and cache it
                     var responseToCache = response.clone();
 
-                    caches.open('my-cache').then(function(cache) {
+                    caches.open(CACHE_NAME).then(function(cache) {
                         cache.put(event.request, responseToCache);
                     });
 
@@ -48,11 +49,21 @@ self.addEventListener('fetch', function(event) {
     );
 });
 
-
-
-
- 
-// Activate Service Worker
-self.addEventListener('activate', event => {
+// Activate New Service Worker and Delete Old Caches
+self.addEventListener('activate', function(event) {
     console.log('Service Worker has been activated');
-})
+    const cacheWhitelist = [CACHE_NAME]; // Keep only the current cache
+
+    event.waitUntil(
+        caches.keys().then(function(cacheNames) {
+            return Promise.all(
+                cacheNames.map(function(cacheName) {
+                    if (!cacheWhitelist.includes(cacheName)) {
+                        return caches.delete(cacheName); // Delete old caches
+                    }
+                })
+            );
+        })
+    );
+    return self.clients.claim(); // Ensure that the active service worker takes control of all pages
+});
